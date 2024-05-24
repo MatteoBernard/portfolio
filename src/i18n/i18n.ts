b2485486465fe046b75d89e93612b64c89efa0c5
Bernard Mattéo <matteo_bernard1@ens.univ-artois.fr>
2024-05-24 20:22:25 +0200

    Setup i18n

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,4 +1,5 @@
-import i18next from "i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import aboutme_en from "./en/aboutme.json";
 import aboutme_fr from "./fr/aboutme.json";
 import skills_en from "./en/skills.json";
@@ -6,19 +7,26 @@ import skills_fr from "./fr/skills.json";
 import projects_en from "./en/projects.json";
 import projects_fr from "./fr/projects.json";
 
-i18next.init({
-    interpolation: { escapeValue: false },
-    lng: "en",
-    resources: {
-        en: {
-            aboutme: aboutme_en,
-            skills: skills_en,
-            projects: projects_en
+const browserLanguage = navigator.language.slice(0, 2);
+
+i18n
+    .use(initReactI18next)
+    .init({
+        interpolation: { escapeValue: false },
+        lng: browserLanguage,
+        fallbackLng: 'en',
+        resources: {
+            en: {
+                aboutme: aboutme_en,
+                skills: skills_en,
+                projects: projects_en,
+            },
+            fr: {
+                aboutme: aboutme_fr,
+                skills: skills_fr,
+                projects: projects_fr,
+            },
         },
-        fr: {
-            aboutme: aboutme_fr,
-            skills: skills_fr,
-            projects: projects_fr
-        }
-    },
-});
+    });
+
+export default i18n;
--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {HashRouter} from "react-router-dom";
 import './i18n';
+import {I18nextProvider} from "react-i18next";
+import i18next from "i18next";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
-        <App />
-    </HashRouter>
+      <I18nextProvider i18n={i18next}>
+          <HashRouter>
+              <App />
+          </HashRouter>
+      </I18nextProvider>
   </React.StrictMode>
 );
 
